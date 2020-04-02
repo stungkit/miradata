43627c049ab856d59d3986eaaa2702559b5227a5
Aerin Kim <aerin.kim@microsoft.com>
2020-04-02 08:28:17 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A suite of tools for generating high quality training and validation data for AI applications.',
+    author='Aerin Kim',
+    license='MIT',
+)
